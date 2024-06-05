3fed11ecaed25c0cdea4f1ae58d0673cff4e4ede
Kacper Arendt <71129131+Kacper-Arendt@users.noreply.github.com>
2024-06-05 16:32:38 +0200

    Add lp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@
 import s from "./styles.module.css"
 import {Map} from "@/components/map/Map";
 export default function Home() {
-  
   return (
       <main className={s.main}
       >
         <p className={s.title}>Generator śladu węglowego imienia Taylor Swift</p>
         <div className={s.bg} />
-        <Map />
+        <Map headerClassname={s.inputs} />
 
       </main>
   );
