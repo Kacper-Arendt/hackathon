e4c66357d3326346a8a89fd491ba61c3afa13f4a
Franciszek Pawlak <box@franciszekpawlak.pl>
2024-06-05 21:34:34 +0200

    add suspense

---
+++ src/app/justice/layout.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {Suspense} from "react";
+
+export default function Layout({
+                                   children,
+                               }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <Suspense>
+            {children}
+        </Suspense>
+    );
+}
