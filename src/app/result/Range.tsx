4da1f8e9b5367ac86813437a2c0c560d6b82be81
ArendtKacper <arendtkacper@gmail.com>
2024-06-06 09:27:16 +0200

    Add range colors

--- src/app/result/Range.tsx
+++ src/app/result/Range.tsx
@@ -1,8 +1,12 @@
+function isValueGreaterThanHalfMax(value: number, max: number) {
+    return value > max / 2;
+}
+
 export const Range = ({min, max, steps, value, label}: {label: string;value: number;min: number; max: number; steps?: number}) => {
     return (
         <div >
             <p>{label}</p>
-            <input type="range" min={min} max={max} value={value} step={steps} className="range range-secondary"/>
+            <input type="range" min={min} max={max} value={value} step={steps} className={`range ${isValueGreaterThanHalfMax(value, max) ? "range-error" : "range-success"} `}/>
             <div className="w-full flex justify-between text-xs">
                 <span>Mały</span>
                 <span>Duży</span>
